e34bd66982bbddb7b3c559cfd55ad43142d24cd8
matthfelix <matheusfelix0996@gmail.com>
2023-12-14 19:28:36 -0300

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-console.log("Digite o nome do seu personagem")
-let nome = "Matheus"
-console.log("parabens menina felizz");
+//variáveis
+
+let nomeHeroi = "Hollow Knight"
+let xp = 5357
+let nivel
+
+//condições
+
+if(xp <= 1000) {
+    nivel = "Ferro"
+}else if(xp >= 1001 && xp <= 2000) {
+	nivel = "Bronze"
+}else if(xp >= 2001 && xp <= 5000) {
+	nivel = "Prata"
+}else if(xp >= 5001 && xp <= 7000) {
+	nivel = "Ouro"
+}else if(xp >= 7001 && xp <= 8000) {
+	nivel = "Platina"
+}else if(xp >= 8001 && xp <= 9000) {
+	nivel = "Diamante"
+}else if(xp >= 9001 && xp <= 10.000) {
+	nivel = "Imortal"
+}else if(xp >= 10001){
+	nivel = "Radiante"
+}
+
+console.log("O herói " + nomeHeroi + ", está no nível " + nivel + "!")
